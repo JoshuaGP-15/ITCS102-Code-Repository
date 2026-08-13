eeeadeb9be48bf1294b2c8025920c325a5a16d7e
JoshuaGP-15 <pauyojoshua25@gmail.com>
2026-08-13 21:41:03 +0800

    Add greeting message to code_challenge1.py

--- code_challenge1.py
+++ code_challenge1.py
@@ -1,1 +1,6 @@
 # ITCS102-Code-Repository
+
+name = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\tHello my name is\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t\t\tJoshua G. Pauyo\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+
+
+print(name)
